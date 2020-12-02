f08044106664eccdd74263b9bacfa624c43b227a
mjudianto <mathewjudianto0603@gmail.com>
2020-12-02 22:07:46 +0700

    2-12-2020

---
+++ app.js
@@ -0,0 +1,12 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+
+modalBtn.addEventListener("click", function(){
+    modalOverlay.classList.add("open-modal")
+});
+
+closeBtn.addEventListener("click", function(){
+    modalOverlay.classList.remove("open-modal")
+});
